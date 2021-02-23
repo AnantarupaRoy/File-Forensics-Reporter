c66edfd34dc708394c1c4771d9288c2876c4308e
AnantarupaRoy <76784729+AnantarupaRoy@users.noreply.github.com>
2021-02-23 18:19:58 +0530

    added strings command and stegseek

--- jpg.py
+++ jpg.py
@@ -2,7 +2,32 @@ import re
 import subprocess
 
 def solve(path):
-	# steghide
+	# strings
+	data = subprocess.Popen(["strings",path], stdout=subprocess.PIPE)
+	output=data.communicate()[0]
+		
+	with open("Report.md","a+b") as f:
+		f.write(output)
+		f.close()
+	
+		
 	# stegseek
+	data = subprocess.Popen(["stegseek",path,"/usr/share/wordlists/rockyou.txt"], stdout=subprocess.PIPE )
+	output=data.communicate()[0]
+	try:	
+		filename=path+".out"
+		with open(filename,"r") as f1:
+			text = f1.read()
+		with open("Report.md","a") as f:
+			f.write("\n\n OUTPUT FROM STEGSEEK : \n\n")
+			f.write(text)
+			f.close()
+	except:
+		with open("Report.md","a") as f:
+			f.write("no valid passphrase using stegseek")
+			f.close()
+				
+	
+
 	# outguess
 	 
