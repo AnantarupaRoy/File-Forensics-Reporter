bfe37b0360229bd0680054b896cd7e155f15f1c4
AnantarupaRoy <76784729+AnantarupaRoy@users.noreply.github.com>
2021-02-24 14:03:58 +0530

    minor changes

--- jpg.py
+++ jpg.py
@@ -5,6 +5,9 @@ def solve(path):
 	# strings
 	data = subprocess.Popen(["strings",path], stdout=subprocess.PIPE)
 	output=data.communicate()[0]
+	with open("Report.md","a") as f:
+		f.write("\n\nOUTPUT FROM STRINGS:\n\n")
+		f.close()	
 		
 	with open("Report.md","a+b") as f:
 		f.write(output)
@@ -28,6 +31,4 @@ def solve(path):
 			f.close()
 				
 	
-
 	# outguess
-	 
