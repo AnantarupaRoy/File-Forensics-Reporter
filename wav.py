1b2eeefbe6e5c9f8fc0f34151ee608c7f6876049
AnantarupaRoy <76784729+AnantarupaRoy@users.noreply.github.com>
2021-02-24 14:05:41 +0530

    Update wav.py
    used strings command, lsb steganography and audio-spectrogram

--- wav.py
+++ wav.py
@@ -2,5 +2,35 @@ import re
 import subprocess
 
 def solve(path):
+	# strings
+	data = subprocess.Popen(["strings",path], stdout=subprocess.PIPE)
+	output=data.communicate()[0]
+	with open("Report.md","a") as f:
+		f.write("\n\nOUTPUT FROM STRINGS:\n\n")
+		f.close()	
+		
+	with open("Report.md","a+b") as f:
+		f.write(output)
+		f.close()
 	# audio-spectro
+	
+	subprocess.run(['sox', path , '-n','spectrogram'])
+	
+		
+	with open("Report.md","a") as f:
+		f.write("A spectrogram of the audio file has been created in spectrogram.png file.\n\n")
+		f.close()
+	
 	# lsb
+	data = subprocess.Popen(["stegolsb","wavsteg","-r","-i",path,"-o","output.txt","-b","10000"], stdout=subprocess.PIPE )
+	output=data.communicate()[0]
+	# try:	
+	filename="output.txt"
+	with open(filename,"rb") as f1:
+		text = f1.read()
+	with open("Report.md","a") as f:
+		f.write("\n\n OUTPUT FROM STEGO-LSB: \n\n")
+		f.close()
+	with open("Report.md","a+b") as f2:
+		f2.write(text)
+	
