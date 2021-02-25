2aea65e6702b1f5b4eda9528073a2d263fb5767b
AnantarupaRoy <76784729+AnantarupaRoy@users.noreply.github.com>
2021-02-25 11:26:40 +0530

    added strings and zsteg

--- png.py
+++ png.py
@@ -2,6 +2,27 @@ import re
 import subprocess
 
 def solve(path):
-	# steghide
-	# lsb
+
+	# strings
+	data = subprocess.Popen(["strings",path], stdout=subprocess.PIPE)
+	output=data.communicate()[0]
+	with open("Report.md","a") as f:
+		f.write("\n\nOUTPUT FROM STRINGS:\n\n")
+		f.close()	
+		
+	with open("Report.md","a+b") as f:
+		f.write(output)
+		f.close()
+
+	
 	# zsteg
+	data = subprocess.Popen(["zsteg", "-a",path], stdout=subprocess.PIPE)
+	output=data.communicate()[0]
+	with open("Report.md","a") as f:
+		f.write("\n\nOUTPUT FROM ZSTEG:\n\n")
+		f.close()
+		
+	with open("Report.md","a+b") as f:
+		f.write(output)
+		f.close()
+	
