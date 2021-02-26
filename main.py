ff77b90f1e0fff716665ea9504ab2e173935ca80
AnantarupaRoy <76784729+AnantarupaRoy@users.noreply.github.com>
2021-02-26 11:22:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,22 +10,21 @@ path = os.path.abspath(file)
 result = subprocess.run(["file",path], stdout=subprocess.PIPE)
 result=str(result.stdout)
 details=re.findall(r'\w+',result)
-print (details)
 
 
 if('PNG' in details):
 	import png
-	print("png")
+	print("png file")
 	png.solve(path)
 		
 elif('JPEG' in details):
 	import jpg
-	print("jpg")
+	print("jpeg file")
 	jpg.solve(path)
 	
 elif('WAVE' in details):
 	import wav
-	print("wav")
+	print("wave file")
 	wav.solve(path)
 else:		
 	print("unrecognised filetype")		
