4df4034bbdd9f765e41d0866c7e397e4872f14e4
AnantarupaRoy <76784729+AnantarupaRoy@users.noreply.github.com>
2021-03-09 22:43:02 +0530

    added outguess

--- jpg.py
+++ jpg.py
@@ -9,37 +9,15 @@ def solve(path):
 
 	
 	# strings
-	data = subprocess.Popen(["strings",path], stdout=subprocess.PIPE)
-	output=data.communicate()[0]
-	with open("Report.md","a") as f:
-		f.write("\n\nOUTPUT FROM STRINGS:\n\n")
-		f.close()	
-		
-	with open("Report.md","a+b") as f:
-		f.write(output)
-		f.close()
+	optional_modules.strings(path)
 	
 		
 	# stegseek
-	data = subprocess.Popen(["stegseek",path,"/usr/share/wordlists/rockyou.txt"], stdout=subprocess.PIPE )
-	output=data.communicate()[0]
-	try:	
-		filename=path+".out"
-		with open(filename,"r") as f1:
-			text = f1.read()
-		with open("Report.md","a") as f:
-			f.write("\n\n OUTPUT FROM STEGSEEK : \n\n")
-			f.write(text)
-			f.close()
-	except:
-		with open("Report.md","a") as f:
-			f.write("no valid passphrase using stegseek")
-			f.close()
+	optional_modules.stegseek(path)
+	# outguess
+	optional_modules.outguess(path)
+	optional_modules.carving(path)
 
 	optional_modules.exiftool(path)
 	optional_modules.binwalk(path)
 	optional_modules.xxd(path)
-				
-	
-	
-	 
