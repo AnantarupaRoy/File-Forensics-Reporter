26bea5affa808bec10ebe2ecd6d0eef2693b2f45
AnantarupaRoy <76784729+AnantarupaRoy@users.noreply.github.com>
2021-03-09 22:49:45 +0530

    added LSB steg

--- png.py
+++ png.py
@@ -7,40 +7,18 @@ def solve(path):
 	optional_modules.stegextract(path)
 
 	# strings
-	data = subprocess.Popen(["strings",path], stdout=subprocess.PIPE)
-	output=data.communicate()[0]
-	with open("Report.md","a") as f:
-		f.write("\n\nOUTPUT FROM STRINGS:\n\n")
-		f.close()	
-		
-	with open("Report.md","a+b") as f:
-		f.write(output)
-		f.close()
+	optional_modules.strings(path)
 
 	# pngcheck
-	data = subprocess.Popen(["pngcheck",path], stdout=subprocess.PIPE)
-	output=data.communicate()[0]
-	with open("Report.md","a") as f:
-		f.write("\n\nOUTPUT FROM PNGCHECK:\n\n")
-		f.close()	
-		
-	with open("Report.md","a+b") as f:
-		f.write(output)
-		f.close()
-
+	optional_modules.pngcheck(path)
 	
 	# zsteg
-	data = subprocess.Popen(["zsteg", "-a",path], stdout=subprocess.PIPE)
-	output=data.communicate()[0]
-	with open("Report.md","a") as f:
-		f.write("\n\nOUTPUT FROM ZSTEG:\n\n")
-		f.close()
-		
-	with open("Report.md","a+b") as f:
-		f.write(output)
-		f.close()
-	
+	optional_modules.zsteg(path)
+
+	# LSB-steg
+	optional_modules.lsb_steg(path)
 	# extras
 	optional_modules.exiftool(path)
 	optional_modules.binwalk(path)
+	optional_modules.carving(path)
 	optional_modules.xxd(path)
