477cd3468054af58e09197430d332d3156b9e625
AnantarupaRoy <76784729+AnantarupaRoy@users.noreply.github.com>
2021-03-09 22:47:16 +0530

    cleaned cluttered code

--- pdf.py
+++ pdf.py
@@ -1,41 +1,13 @@
-import re
-import subprocess
-import optional_modules
-
 def solve(path):
 	# pdf-parser
-	data = subprocess.Popen(["pdf-parser","-a",path], stdout=subprocess.PIPE)
-	output=data.communicate()[0]
-	with open("Report.md","a") as f:
-		f.write("\n\nOUTPUT FROM PDF-PARSER:\n\n")
-		f.close()	
-		
-	with open("Report.md","a+b") as f:
-		f.write(output)
-		f.close()
+	optional_modules.pdf_parser(path)
 
 
 	# strings
-	data = subprocess.Popen(["strings",path], stdout=subprocess.PIPE)
-	output=data.communicate()[0]
-	with open("Report.md","a") as f:
-		f.write("\n\nOUTPUT FROM STRINGS:\n\n")
-		f.close()	
-		
-	with open("Report.md","a+b") as f:
-		f.write(output)
-		f.close()
+	optional_modules.strings(path)
 
 	# carving
-	data = subprocess.Popen(["foremost","-T",path,"-v"], stdout=subprocess.PIPE)
-	output=data.communicate()[0]
-	with open("Report.md","a") as f:
-		f.write("\n\nOUTPUT FROM FOREMOST:\n\n")
-		f.close()	
-		
-	with open("Report.md","a+b") as f:
-		f.write(output)
-		f.close()
+	optional_modules.carving(path)
 
 	optional_modules.exiftool(path)
 	optional_modules.binwalk(path)
