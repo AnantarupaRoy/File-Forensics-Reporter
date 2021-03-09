6c18f99c8959f022a431a59e77017cffa3c73777
AnantarupaRoy <76784729+AnantarupaRoy@users.noreply.github.com>
2021-03-09 22:41:32 +0530

    cleaned code

--- bmp.py
+++ bmp.py
@@ -1,17 +1,17 @@
+
 import optional_modules
 import subprocess
 
 def solve(path):
 	# strings
-	data = subprocess.Popen(["strings",path], stdout=subprocess.PIPE)
-	output=data.communicate()[0]
-	with open("Report.md","a") as f:
-		f.write("\n\nOUTPUT FROM STRINGS:\n\n")
-		f.close()	
-		
-	with open("Report.md","a+b") as f:
-		f.write(output)
-		f.close()
+	optional_modules.strings(path)
+
+	# stegseek
+	optional_modules.stegseek(path)
+	# LSB-steg
+	optional_modules.lsb_steg(path)
+	optional_modules.carving(path)
+
 
 	optional_modules.exiftool(path)
 	optional_modules.binwalk(path)
