a464877fe8389536ee05c7d843059955bd88e1fe
AnantarupaRoy <76784729+AnantarupaRoy@users.noreply.github.com>
2021-03-10 19:57:44 +0530

    Update text.py

--- text.py
+++ text.py
@@ -4,6 +4,7 @@ import optional_modules
 def solve(path):
 	# strings
 	optional_modules.strings(path)
+	optional_modules.stegsnow(path)
 	optional_modules.carving(path)
 
 	optional_modules.exiftool(path)
