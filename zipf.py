2e54b2a1e4690be86d6584c25bd9ee13c819541f
AnantarupaRoy <76784729+AnantarupaRoy@users.noreply.github.com>
2021-04-02 18:38:23 +0530

    Update zipf.py

--- zipf.py
+++ zipf.py
@@ -13,27 +13,23 @@ def solve(path):
 			zip.extractall() 
 
 	except:
-		BUF_SIZE = 32768 
-		md5 = hashlib.md5()
-		sha1 = hashlib.sha1()
-		with open("Report.md","a")as f1:
-			with open(path, 'rb') as f:
-
-				while True:
-					data = f.read(BUF_SIZE)
-					if not data:
-					  break
-					md5.update(data)
-					sha1.update(data)
-			f1.write("MD5: {0}".format(md5.hexdigest())+"\n")
-			f1.write("SHA1: {0}".format(sha1.hexdigest()))
-			f.close()
+		data = subprocess.Popen(["zip2john",path,">","hash"], stdout=subprocess.PIPE)
+		output=data.communicate()[0]
+		with open("hash","r") as f1:
+			hashvalue=f1.read()
+			with open("Report.md","a") as f2:
+				f2.write("\n\nHASH VALUE FROM JOHN:\n\n")
+				f2.write(hashvalue+"\n")
+			f2.close()
 		f1.close()
+
 		data = subprocess.Popen(["fcrackzip","-u","-D","-p","/usr/share/wordlists/rockyou.txt",path], stdout=subprocess.PIPE)
 		output=data.communicate()[0]
 		with open("Report.md","a+b") as f:
 			f.write(output)
 			f.close()
 
+
+ 	
 
     	
