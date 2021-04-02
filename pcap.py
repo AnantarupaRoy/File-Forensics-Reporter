50afc2bad08128ea69a2093eb23e26c4e978decf
AnantarupaRoy <76784729+AnantarupaRoy@users.noreply.github.com>
2021-04-02 12:36:03 +0530

    Update pcap.py

--- pcap.py
+++ pcap.py
@@ -1,34 +1,7 @@
-from kamene.all import *
 import subprocess
-import scapy
+import optional_modules
 
 def solve(path):
 	
-	pcap = rdpcap(path)
-	with open ("Report.md","a+") as f:
-		f.write("UDP:\n\n")
-		
-		print(pcap)
-		f.close()
-	for p in pcap[UDP]:
-		try:
-			with open ("Report.md","a+b") as f:
-				if "xml version" in  p[Raw].load.decode():
-					continue
-				else:
-					f.write(p[Raw].load)
-		
-		except:
-			continue
-
-	with open ("Report.md","a+") as f:
-		f.write("\n\nTCP:\n\n")
-		f.close()
-	for p in pcap[TCP]:
-		try:
-			with open ("Report.md","a+b") as f:
-				f.write(p[Raw].load)
-		
-		except:
-			continue
-	
+	optional_modules.binwalk(path)
+	optional_modules.carving(path)
