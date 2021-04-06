b0865f7286459b8fc01915bdf3f91d3661a7bce4
AnantarupaRoy <76784729+AnantarupaRoy@users.noreply.github.com>
2021-04-06 22:10:27 +0530

    formatted zipf.py

--- zipf.py
+++ zipf.py
@@ -13,22 +13,23 @@ def solve(path):
 			zip.extractall() 
 
 	except:
-		data = subprocess.Popen(["zip2john",path,">","hash"], stdout=subprocess.PIPE)
+		data = subprocess.Popen(["zip2john",path], stdout=subprocess.PIPE)
 		output=data.communicate()[0]
-		with open("hash","r") as f1:
-			hashvalue=f1.read()
-			with open("Report.md","a") as f2:
-				f2.write("\n\nHASH VALUE FROM JOHN:\n\n")
-				f2.write(hashvalue+"\n")
-			f2.close()
-		f1.close()
+		# with open("hash","r") as f1:
+		for line in output.split(b"\n"):
+			if (re.search(path,line.decode()) or re.search(r"\*",line.decode()) and (not "ver" in line.decode())):
+				hashvalue=line.decode()
+				with open("Report.md","a") as f2:
+					f2.write("\n\nHASH VALUE FROM JOHN:\n\n")
+					f2.write(hashvalue+"\n")
+				f2.close()
+		# f1.close()
 
 		data = subprocess.Popen(["fcrackzip","-u","-D","-p","/usr/share/wordlists/rockyou.txt",path], stdout=subprocess.PIPE)
 		output=data.communicate()[0]
 		with open("Report.md","a+b") as f:
 			f.write(output)
 			f.close()
-
 
  	
 
